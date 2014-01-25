ee902f79ca3048bc830120ff92d8f27d75266572
patrickgh3 <patrickjtraynor@gmail.com>
2014-01-25 02:05:59 -0800

    General prefabs and cleanup

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -5,14 +5,29 @@ public class Character : MonoBehaviour {
     public float jumpSpeed = 5f;
     public float moveSpeed = 2.5f;
 
+    Vector2 moveDir;
+    bool shouldJump = false;
+
     public void move(float speed)
     {
-        rigidbody2D.velocity = new Vector2(speed * moveSpeed, rigidbody2D.velocity.y);
+        moveDir = new Vector2(speed * moveSpeed, rigidbody2D.velocity.y);
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody2D.velocity = moveDir;
+
+        if (shouldJump)
+        {
+            if (Math.Abs(rigidbody2D.velocity.y) <= 0.001f)
+                rigidbody2D.velocity += new Vector2(0f, jumpSpeed);
+
+            shouldJump = false;
+        }
     }
 
     public void jump()
     {
-        if(Math.Abs(rigidbody2D.velocity.y) <= 0.001f)
-            rigidbody2D.velocity += new Vector2(0f, jumpSpeed);
+        shouldJump = true;
     }
 }
