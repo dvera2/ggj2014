32e5e699470dd5a012afa217076496d3f02b0ad8
Dave Vera <dvera2@gmail.com>
2014-01-25 10:11:04 -0800

    Modified LevelLoader to generate tilesets within the editor without pressing Play. (Click the generateSet boolean box to load the tileset XML)

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -4,21 +4,76 @@ using System.Collections.Generic;
 using System.Xml;
 using System.IO;
 
+[ExecuteInEditMode]
 public class LevelLoader : MonoBehaviour {
 
 	public TextAsset LevelXMLFile;
 	public GameObject Tile;
 	public Sprite[] TileSprites;
 
+	public bool generateSet = false;
+
 	private const float tileScale = 0.24f;
+	private Transform tileSet;
+
+	/// <summary>
+	/// Attaches to or creates a collection transform to store generated tiles.
+	/// </summary>
+	/// <returns>The tile set transform.</returns>
+	Transform TileSet() {
+		Transform tileSet = transform.FindChild ("TileSet");
+		if (!tileSet) {
+			GameObject t = new GameObject("TileSet");
+			t.transform.parent = transform;
+			t.transform.localPosition = Vector3.zero;
+			t.transform.localRotation = Quaternion.identity;
+			t.transform.localScale = Vector3.one;
+			
+			tileSet = t.transform;
+		}
+		return tileSet;
+	}
+
+	void Start() {
+
+	}
+
+	void Update() {
+	    if (generateSet) {
+			generateSet = false;
+
+			ClearChildren();
+			Load ();
+		}
+	}
+
+	/// <summary>
+	/// Removes all child transforms of this object.
+	/// </summary>
+	/// <returns>The children.</returns>
+	void ClearChildren() {
+		Transform tileSet = TileSet ();
+		Transform t;
+		for (int i = tileSet.childCount - 1; i >= 0; i--) {
+			t = tileSet.GetChild(i);
+
+			if(Application.isEditor) {
+				DestroyImmediate(t.gameObject);
+			} else {
+				Destroy(t.gameObject);
+			}
+		}
+	}
 
 	// Use this for initialization
-	void Start () 
+	void Load () 
 	{
 		XmlDocument xmlDoc = new XmlDocument ();
 		xmlDoc.LoadXml (LevelXMLFile.text);
 
 		XmlNodeList levelList = xmlDoc.GetElementsByTagName ("level");
+
+		Transform tileSet = TileSet ();
 
 		foreach (XmlNode level in levelList) 
 		{
@@ -49,6 +104,7 @@ public class LevelLoader : MonoBehaviour {
 						y *= tileScale;
 
 						GameObject newTile = GameObject.Instantiate(Tile, new Vector3(x, y, 0.0f), Quaternion.identity) as GameObject;
+						newTile.transform.parent = tileSet.transform;
 
 						if(tileId >= 0 && tileId < TileSprites.Length)
 							newTile.GetComponent<SpriteRenderer>().sprite = TileSprites[tileId];
@@ -57,11 +113,5 @@ public class LevelLoader : MonoBehaviour {
 			}
 		}
 
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-	
 	}
 }
