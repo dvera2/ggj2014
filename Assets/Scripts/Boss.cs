1dc5f280c39e1ffb6c45d3475e4e5bab240ed953
Jeffrey Johnson <jeffcjohnson@live.com>
2014-01-26 02:59:26 -0800

    More boss scripts

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -4,14 +4,30 @@ public class Boss : MonoBehaviour {
     public float projectileDelay = 2f;
     private float delayTimer = 0f;
     public Transform projectile;
+    private bool dying = false;
+    private Vector2 velocity = new Vector2(0f, 0f);
 	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update () {
         delayTimer += Time.deltaTime;
-        if(delayTimer >= projectileDelay)
+        if(!dying && delayTimer >= projectileDelay)
         {
             delayTimer = 0f;
             Instantiate(projectile, transform.position, Quaternion.identity);
         }
+        else if (dying)
+        {
+            velocity.x += Random.Range(-.4f, .4f);
+            transform.Translate(velocity);
+        }
 	}
+
+    void OnTiggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag != "Player")
+        {
+            velocity.y = -3f;
+            dying = true;
+        }
+    }
 }
