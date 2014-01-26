1a0d7f167875e576a9376d2dbbd4e2996e9052bf
Dave Vera <dvera2@gmail.com>
2014-01-25 19:56:15 -0800

    Adding in background assets and prefabs.

---
+++ Assets/Scripts/RotateObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateObject : MonoBehaviour {
+
+	public float rotationRate = 20.0f;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround(transform.position, Vector3.forward, rotationRate * Time.deltaTime);
+	}
+}
