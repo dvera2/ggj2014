f881cc8f929c703548cf5de44baf3ae021cc3411
vishtheshnu <vishtheshnu@gmail.com>
2014-01-25 17:24:19 -0800

    Spawn Point & Checkpoint
    Created and implemented a spawn point and checkpoint. Player teleports
    to spawn point at beginning of level when its Start() is called. When
    player passes checkpoint, static position stored in SpawnPointScript =
    location of checkpoint. When the player dies (in player death script),
    it calls the SpawnPointScript to teleport the player to the last
    checkpoint.

---
+++ Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnScript : MonoBehaviour {
+
+	public GameObject player;
+	public static Vector3 spawnpos;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("SpawnScript running!");
+		player = GameObject.FindGameObjectWithTag ("Player");
+		player.transform.position = (transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//player = GameObject.FindGameObjectWithTag ("Player");
+		//player.transform.TransformPoint (transform.position);
+	}
+
+	/**
+		Called when the player dies or the level is reset;
+		sets the player's position to spawnpos
+	 */
+	public static void levelReset(){
+		player.transform.TransformPoint (spawnpos);
+	}
+}
---
+++ Assets/Scripts/checkpointscript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+	This class represents a checkpoint. When the player makes contact with it,
+	it saves their location. In the event of death/level reset, the player's
+	location is set to the location of the CheckPoint prefab
+ */
+public class CheckPointScript : MonoBehaviour {
+
+	public GameObject player; //Player
+	public bool isReached; //Whether the checkpoint has been reached
+
+	// Use this for initialization
+	void Start () {
+		//Instantiate vars
+		player = GameObject.FindGameObjectWithTag("Player");
+		//Init variables
+		isReached = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Check if player overlaps with checkpoint
+		//Debug.Log(player.transform.position);
+		//if so, and the checkpoint hasn't already been used, set it as the proper checkpoint
+		if (player != null && !isReached && player.transform.position.x >= transform.position.x) {
+			isReached = true; //set reached bool to true
+			SpawnScript.spawnpos = transform.position; //Set SpawnScript's spawnpos to this CheckPoint's position
+			Debug.Log("Checkpoint Reached!"); 
+		}
+	}
+}
