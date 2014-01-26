670384b9b0444ae326b3facf8eb8d8a707777626
Dave Vera <dvera2@gmail.com>
2014-01-26 02:24:25 -0800

    Added scripts to fix background/camera/player interaction.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+	public float smoothTime = 0.3f;
+
+	float currVelocity;
+	float vel, velY;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		var pos = transform.position;
+		pos.x = Mathf.SmoothDamp(pos.x, target.position.x, ref vel, smoothTime);
+		pos.y = Mathf.SmoothDamp(pos.y, target.position.y, ref velY, smoothTime);
+		Debug.Log(string.Format("{0}, {1}", pos.x, pos.y));
+		transform.position = pos;
+
+		Debug.Log (string.Format ("{0}, {1}", transform.position.x, transform.position.y));
+	}
+}
---
+++ Assets/Scripts/FollowTheCam.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowTheCam : MonoBehaviour {
+	public Camera cam;
+
+	// Use this for initialization
+	void Start () {
+		if (!cam) {
+			var obj = GameObject.FindGameObjectWithTag ("MainCamera");
+			if(obj) cam = obj.GetComponent<Camera>();
+		}
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		float z = transform.position.z;
+		var pos = cam.transform.position;
+		pos.z = z;
+		transform.position = pos;
+	}
+}
