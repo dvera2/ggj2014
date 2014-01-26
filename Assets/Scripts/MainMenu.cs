0bbaa16d181e8da633e0b06a711bfdb6d5601bea
Jon Ross <jonross88@gmail.com>
2014-01-25 21:20:04 -0800

    Main menu scene and play button

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+		if (GUI.Button (new Rect(Screen.width / 2 - 50, Screen.height / 2 - 20, 100, 40), "Play")) 
+		{
+			//game.Start();
+		}
+	}
+}
