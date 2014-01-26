7ebf3afb3999f92374aa2a6a33bd3189b16c86b6
patrickgh3 <patrickjtraynor@gmail.com>
2014-01-26 01:16:48 -0800

    Added end of level warps

---
+++ Assets/Scripts/EndLevelTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndLevelTrigger : MonoBehaviour {
+
+	void Start () {
+        gameObject.renderer.enabled = false;
+	}
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Player")
+        {
+            GameObject.Destroy(gameObject);
+            StateManager.nextLevel();
+        }
+    }
+}
