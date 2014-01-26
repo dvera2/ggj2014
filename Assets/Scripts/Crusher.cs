8de0cb87d3eb0822887b2671900a1d95beead761
Jeffrey Johnson <jeffcjohnson@live.com>
2014-01-26 13:52:15 -0800

    Fixed Crusher

--- Assets/Scripts/Crusher.cs
+++ Assets/Scripts/Crusher.cs
@@ -6,24 +6,28 @@ public class Crusher : MonoBehaviour {
         UP, DOWN, LEFT, RIGHT
     }
     public Direction direction = Direction.UP;
-    public float speed = .5f;
-    public float delay = 2f;
+    public float speed = 1f;
+    public float delay = 1f;
     private float crushTimer = 0f;
 	
 	// Update is called once per frame
     void Update()
     {
         crushTimer += Time.deltaTime;
-        if(crushTimer >= delay)
+        if (crushTimer >= 3 * delay) crushTimer = 0f;
+        else if(crushTimer >= 2 * delay)
         {
-            if (direction == Direction.UP) transform.Translate(0f, Time.deltaTime / speed, 0f);
-            if (direction == Direction.DOWN) transform.Translate(0f, -(Time.deltaTime / speed), 0f);
-            if (direction == Direction.LEFT) transform.Translate(0f, -(Time.deltaTime / speed), 0f);
-            if (direction == Direction.RIGHT) transform.Translate(0f, Time.deltaTime / speed, 0f);
+            if (direction == Direction.UP) transform.Translate(0f, -Time.deltaTime * speed, 0f);
+            if (direction == Direction.DOWN) transform.Translate(0f, (Time.deltaTime * speed), 0f);
+            if (direction == Direction.LEFT) transform.Translate(0f, (Time.deltaTime * speed), 0f);
+            if (direction == Direction.RIGHT) transform.Translate(0f, -Time.deltaTime * speed, 0f);
         }
-        else if(crushTimer >= speed + delay)
+        else if(crushTimer >= delay)
         {
-            crushTimer = 0f;
+            if (direction == Direction.UP) transform.Translate(0f, Time.deltaTime * speed, 0f);
+            if (direction == Direction.DOWN) transform.Translate(0f, -(Time.deltaTime * speed), 0f);
+            if (direction == Direction.LEFT) transform.Translate(0f, -(Time.deltaTime * speed), 0f);
+            if (direction == Direction.RIGHT) transform.Translate(0f, Time.deltaTime * speed, 0f);
         }
 	}
 }
